8944211b46d1596a184ceb936209093d447835ac
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2022-01-09 22:40:56 +0100

    Handle error of non-existent coin

--- src/components/SearchCoin.js
+++ src/components/SearchCoin.js
@@ -1,23 +1,39 @@
 import React, { useState } from 'react';
+import getCoin from '../helpers/getCoin';
 
 const SearchCoin = () => {
 
   const [coinItem, setCoinItem] = useState('')
+  const [error, setError] = useState('')
 
   const handleSearch = (e) => {
     setCoinItem(e.target.value)
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
 
     let trimmedCoin = coinItem.trim()
-    trimmedCoin = trimmedCoin.replace(/\s/g, '-')
+    if (trimmedCoin.length > 0) {
+      trimmedCoin = trimmedCoin.replace(/\s/g, '-')
+      const data = await getCoin(trimmedCoin)
+      console.log(data)
+      if (data.error) {
+        setError(data.error)
+      } else {
+        console.log('Coin exists')
+        setError('')
+      }
+    } else {
+      console.log('Trimmed: ', trimmedCoin.length)
+    }
+    
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" name="coinItem" value={coinItem} onChange={handleSearch} />
+      {error}
       <button type='submit'>Search Coin</button>
     </form>
   );
