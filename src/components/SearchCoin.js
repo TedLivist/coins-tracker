36102d116fb11c2a8e7a4ec03beb252353c20ee9
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2022-01-10 21:37:29 +0100

    Add restriction for tracking specific number of coins

--- src/components/SearchCoin.js
+++ src/components/SearchCoin.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import getCoin from '../helpers/getCoin';
 import { trackCoin } from '../redux/coins/coins';
 
-const SearchCoin = () => {
+const SearchCoin = (props) => {
 
   const dispatch = useDispatch()
 
   const [coinItem, setCoinItem] = useState('')
   const [error, setError] = useState('')
+
+  const { trackedCoins } = props
+  console.log('Tracked Coins: ', trackedCoins)
+  console.log(trackedCoins.length)
 
   const handleSearch = (e) => {
     setCoinItem(e.target.value)
@@ -21,13 +25,14 @@ const SearchCoin = () => {
     if (trimmedCoin.length > 0) {
       trimmedCoin = trimmedCoin.replace(/\s/g, '-')
       const data = await getCoin(trimmedCoin)
-      console.log(data)
       if (data.error) {
         setError(data.error)
-      } else {
+      } else if (trackedCoins.length < 3) {
         dispatch(trackCoin(data))
-        console.log('Coin exists')
+        console.log('Coin has been added')
         setError('')
+      } else {
+        setError("Can't add more than 3 coins")
       }
     } else {
       setError('Cannot search meaningless coin')
