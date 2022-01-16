ce039840e56c7675c7b1e99345324a063fe7dd11
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2022-01-16 18:59:59 +0100

    Use TrackingButton component in OneCoin

--- src/components/OneCoin.js
+++ src/components/OneCoin.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import TrackingButton from './TrackingButton';
 
 const OneCoin = (props) => {
 
   const { coinId } = props
 
+  const handleUntracking = () => {
+    console.log('Incoming untracking...')
+  }
+
   return (
     <div>
-      <p>{coinId}</p>
+      <p>{coinId} <TrackingButton trackingFunc={handleUntracking} buttonText='Untrack this coin' /> </p>
     </div>
   );
 }
