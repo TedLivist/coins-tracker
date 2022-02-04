d3f69d498ec85ae23eb95503171ca9c3f34a2491
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2022-02-04 15:07:45 +0100

    Add portfolio worth to Coins page

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -5,9 +5,15 @@ import OneCoin from './OneCoin';
 const Coins = () => {
   const coins = useSelector(state => state.coins)
 
+  const worth = coins.filter(coin => coin.qty > 0)
+    .map((coin) => ( coin.qty * coin.market_data.current_price.usd ))
+    .reduce((a, b) => ( a + b ), 0)
+    .toFixed(3)
+  
   return (
     <div>
       <h2>The Coins page</h2>
+      <h4>Portfolio worth: ${worth}</h4>
         {coins.map((coin) => (
           <OneCoin
             key={coin.id}
