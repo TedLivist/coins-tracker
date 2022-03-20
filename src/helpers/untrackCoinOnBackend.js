36264ad505198004736f5871a00a84c71bc44327
TedLivist <livistteddy@gmail.com>
2022-03-20 14:32:56 +0100

    Implement untracking of coin from backend

---
+++ src/helpers/untrackCoinOnBackend.js
@@ -0,0 +1,17 @@
+export const untrackCoinOnBackend = async (userToken, coinId) => {
+  console.log(userToken)
+  const response = await fetch(`http://localhost:3000/api/v1/coins/${coinId}`, {
+    method: 'DELETE',
+    mode: 'cors',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `bearer ${userToken}`
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+  })
+
+  const deleteMessage = await response.json()
+  return deleteMessage
+}
