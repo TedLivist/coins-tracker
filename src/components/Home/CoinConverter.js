6ea13a43be093038d3dddb7a4d3c192ed2dbf612
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2022-04-03 22:59:54 +0100

    Set up input conversion for coin input

--- src/components/Home/CoinConverter.js
+++ src/components/Home/CoinConverter.js
@@ -6,19 +6,41 @@ const CoinConverter = (props) => {
   const {coinPrice, otherCurrencies} = props
   const [selectState, setSelectState] = useState([])
 
+  const [coinValue, setCoinValue] = useState(0)
+  const [currencyValue, setCurrencyValue] = useState(0)
+  const [selectValue, setSelectValue] = useState('')
+
   useEffect(() => {
     setSelectState(coinSelect(otherCurrencies))
   }, [])
 
-  return (
-    <div>
-      <input type='number' />
+  const handleCoinChange = (e) => {
+    setCoinValue(e.target.value)
+    const calc = e.target.value * selectValue
+    setCurrencyValue(calc.toFixed(4))
+  }
 
-      <select>
-        {selectState.map(({item, itemValue}) => (
-          <option key={item} value={itemValue}>{item.toUpperCase()}</option>        
-        ))}
-      </select>
+  const handleSelectChange = (e) => {
+    setSelectValue(e.target.value)
+  }
+
+  const handleCurrencyChange = (e) => {
+    console.log(e.target.value)
+  }
+
+  return (
+    <div className='converter-wrapper'>
+      <input type='number' onChange={handleCoinChange} value={coinValue} />
+
+      <div className='currency-input'>
+        <select onChange={handleSelectChange}>
+          <option>---Select Currency---</option>
+          {selectState.map(({item, itemValue}) => (
+            <option key={item} value={itemValue}>{item.toUpperCase()}</option>        
+          ))}
+        </select>
+        <input type='number' onChange={handleCurrencyChange} value={currencyValue} />
+      </div>
     </div>
   );
 }
