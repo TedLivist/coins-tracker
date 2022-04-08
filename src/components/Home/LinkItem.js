5b32338c98591088f1eb940537be7bcd749f93a4
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2022-04-08 13:33:11 +0100

    Implement copy to clipboard

--- src/components/Home/LinkItem.js
+++ src/components/Home/LinkItem.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 const LinkItem = (props) => {
 
   const {title, link, mainLink} = props
+  const [copied, setCopied] = useState(false)
+
+  const handleCopy = () => {
+    setCopied(true)
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (copied) setCopied(false)
+    }, 2000)
+  })
 
   return (
     <div className='grid grid-cols-2'>
       <div>{title}</div>
       
       <div className='text-right'>
-        <span className='hover:bg-slate-400 hover:rounded-lg hover:p-1 hover:cursor-pointer'>{link}</span>
+        <CopyToClipboard text={mainLink === undefined ? link : mainLink} onCopy={handleCopy}>
+          <div>
+            <span className='hover:bg-slate-400 hover:rounded-lg hover:p-1 hover:cursor-pointer'>{link}</span>
+            <div>{copied && <small>Copied</small>}</div>
+          </div>
+        </CopyToClipboard>
       </div>
     </div>
   );
