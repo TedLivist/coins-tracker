359714c46d5301ad5080cfcd7478a7cfcfa5b765
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2022-04-08 15:44:24 +0100

    Style Tooltip

--- src/components/Home/LinkItem.js
+++ src/components/Home/LinkItem.js
@@ -1,9 +1,21 @@
 import { Tooltip } from '@material-ui/core';
 import { Fade } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 import React, { useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
+const useStyles = makeStyles(theme => ({
+  customTooltip: {
+    backgroundColor: 'black',
+  },
+  customArrow: {
+    color: 'black',
+  },
+}))
+
 const LinkItem = (props) => {
+
+  const classes = useStyles()
 
   const {title, link, mainLink} = props
   const [copied, setCopied] = useState(false)
@@ -20,9 +32,15 @@ const LinkItem = (props) => {
         <CopyToClipboard text={mainLink === undefined ? link : mainLink} onCopy={handleCopy}>
           <div>
             <Tooltip
+              classes={{
+                tooltip: classes.customTooltip,
+                arrow: classes.customArrow
+              }}
               TransitionComponent={Fade}
               TransitionProps={{timeout: 600}}
-              title={copied ? 'Copied' : 'Copy'}
+              title={<span className='font-bold text-base'>{copied ? 'Copied' : 'Copy'}</span>}
+              placement="top"
+              arrow
             >
               <span className='hover:bg-slate-400 hover:rounded-lg hover:p-1 hover:cursor-pointer'>{link}</span>
             </Tooltip>
