a8e9857e98124c45b51f9ba61c97238bb0485351
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2022-04-08 14:22:57 +0100

    Add tooltip with material tailwind

--- src/components/Home/LinkItem.js
+++ src/components/Home/LinkItem.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
+import { Button, Tooltips } from '@material-tailwind/react';
+import { TooltipsContent } from '@material-tailwind/react';
 
 const LinkItem = (props) => {
 
@@ -16,15 +18,35 @@ const LinkItem = (props) => {
     }, 2000)
   })
 
+  const copyRef = useRef()
+
   return (
     <div className='grid grid-cols-2'>
-      <div>{title}</div>
+      <div>
+        {title}
+        {/* <Button className="bg-red-600 text-amber-100" ripple="light" ref={copyRef}>
+          Tooltips
+        </Button>
+
+        <Tooltips placement="top" ref={copyRef}>
+          <TooltipsContent className="bg-yellow-50">Tio</TooltipsContent>
+        </Tooltips> */}
+      </div>
       
       <div className='text-right'>
         <CopyToClipboard text={mainLink === undefined ? link : mainLink} onCopy={handleCopy}>
           <div>
-            <span className='hover:bg-slate-400 hover:rounded-lg hover:p-1 hover:cursor-pointer'>{link}</span>
-            <div>{copied && <small>Copied</small>}</div>
+            <span ref={copyRef} className='hover:bg-slate-400 hover:rounded-lg hover:p-1 hover:cursor-pointer'>{link}</span>
+            
+            <Tooltips placement="top" ref={copyRef}>
+              {!copied && (
+                <TooltipsContent className="bg-black p-1 font-bold text-white">Copy</TooltipsContent>
+              )}
+              {copied && (
+                <TooltipsContent className="bg-black p-1 font-bold text-white">Copied!</TooltipsContent>
+              )}
+            </Tooltips>
+            {/* <div>{copied && <small className='text-sky-600 '>Copied</small>}</div> */}
           </div>
         </CopyToClipboard>
       </div>
