622d19c9af31004f2d04f48c8ab94da7987a4e0e
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2022-04-09 22:03:01 +0100

    Add table to Markets and OneMarket

--- src/components/Home/Markets.js
+++ src/components/Home/Markets.js
@@ -9,33 +9,46 @@ const Markets = (props) => {
 
   return (
     <div>
-      {markets.map((market) => {
-        let base = ''
-        let target = ''
-        if (market.base !== market.coin_id.toUpperCase()) {
-          base = market.coin_id.toUpperCase()
-        } else {
-          base = market.base
-        }
+      <table className='table-auto border-collapse'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Pair</th>
+            <th>Price</th>
+            <th>Volume</th>
+          </tr>
+        </thead>
+        <tbody>
+          {markets.map((market) => {
+            let base = ''
+            let target = ''
+            if (market.base !== market.coin_id.toUpperCase()) {
+              base = market.coin_id.toUpperCase()
+            } else {
+              base = market.base
+            }
 
-        if (market.target.length > 7) {
-          target = market.target_coin_id.toUpperCase()
-        } else {
-          target = market.target
-        }
+            if (market.target.length > 7) {
+              target = market.target_coin_id.toUpperCase()
+            } else {
+              target = market.target
+            }
 
-        return (
-          <OneMarket
-            key={market.bid_ask_spread_percentage}
-            base={base}
-            target={target}
-            name={market.market.name}
-            price={market.converted_last.usd.toFixed(3)}
-            volume={market.converted_volume.usd.toFixed(2)}
-            trade_link = {market.trade_url}
-          />
-        )
-      })}
+            return (
+              <tr key={market.bid_ask_spread_percentage}>
+                <OneMarket
+                  base={base}
+                  target={target}
+                  name={market.market.name}
+                  price={market.converted_last.usd.toFixed(3)}
+                  volume={market.converted_volume.usd.toFixed(2)}
+                  trade_link = {market.trade_url}
+                />
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/components/Home/OneMarket.js
+++ src/components/Home/OneMarket.js
@@ -3,9 +3,12 @@ import React from 'react';
 const OneMarket = (props) => {
   const {name, base, target, volume, price, trade_link} = props
   return (
-    <div>
-      {name} | {base} | {target} | {price} | {`$${volume}`}
-    </div>
+    <>
+      <th className='text-left'>{name}</th>
+      <td className='text-right'>{base}/{target}</td>
+      <td className='text-right'>{price}</td>
+      <td className='text-right'>{`$${volume}`}</td>
+    </>
   );
 }
  
