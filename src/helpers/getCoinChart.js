0ea256da47f13b01a538e7ace77222427a7a6c5c
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2022-04-12 19:52:29 +0100

    Filter Weekly chart for monthly and fix day bug

--- src/helpers/getCoinChart.js
+++ src/helpers/getCoinChart.js
@@ -4,17 +4,19 @@ export const getCoinChart = async (coinId, vsCurrency, days) => {
   const response = await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${vsCurrency}&days=${days}`)
   const data = await response.json()
 
-  let someValues = []
+  let filteredValues = []
   const dataArray = data.prices
   if (days == 1) {
-    someValues = dataArray.filter((item) => (dataArray.indexOf(item) % 12) == 0)
+    filteredValues = dataArray.filter((item) => (dataArray.indexOf(item) % 12) == 0)
+  } else if (days == 30) {
+    filteredValues = dataArray.filter((item) => (dataArray.indexOf(item) % 30) == 0)
   }
 
   // const dataArray = data.prices.slice(-20)
-  const prices = someValues.map((arr) => {
+  const prices = filteredValues.map((arr) => {
     return Number(arr[1].toFixed(3))
   })
-  const timeStamps = someValues.map((arr) => {
+  const timeStamps = filteredValues.map((arr) => {
     return unixTimeConverter(arr[0])
   })
 
--- src/helpers/unixTimeConverter.js
+++ src/helpers/unixTimeConverter.js
@@ -1,19 +1,29 @@
 export const unixTimeConverter = (unixTimestamp) => {
   let date = new Date(unixTimestamp)
+  
+  let month = date.toLocaleDateString('en-US', {month: 'short'})
+
+  let year = date.getFullYear()
+
+  let dayNum = date.toLocaleDateString('en-NG', {day: 'numeric'})
+
   let hours = date.getHours()
   if (hours.toString().length === 1) {
-    hours = '0' + date.getHours()
+    hours = '0' + hours
   }
+
   let minutes = date.getMinutes()
   if (minutes.toString().length === 1) {
-    minutes = '0' + date.getMinutes()
+    minutes = '0' + minutes
   }
+
   let seconds = date.getSeconds()
   if (seconds.toString().length === 1) {
-    seconds = '0' + date.getSeconds()
+    seconds = '0' + seconds
   }
+
   let day = date.toLocaleDateString('en-US', {weekday: 'short'})
   
-  const formatDate = day + ' ' + hours + ':' + minutes + ':' + seconds
+  const formatDate = day + ' ' + dayNum + ' ' + month + ' ' + year + ' ' + hours + ':' + minutes + ':' + seconds
   return formatDate
 }
