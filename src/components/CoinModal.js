8b3f44bc3f9fd79e254c03e4604e73e0f6ea2985
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2022-05-01 04:31:27 +0100

    Style Modal a bit

--- src/components/CoinModal.js
+++ src/components/CoinModal.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch } from 'react-redux';
 import { updateBackendQuantity } from '../helpers/backendMods/updateBackendQuantity';
 import { changeQuantity } from '../redux/coins/coins';
@@ -26,13 +28,20 @@ const CoinModal = ({ open, coinId, backendCoinId, quantity, userToken, onClose }
   if (!open) return null
   return (
     <div>
-      <button onClick={onClose}>Close Modal</button>
-      <div>{coinId}</div>
-      <div>{quantity}</div>
-      <form onSubmit={handleModalSubmit}>
-        <input type="number" step='any' onChange={handleModalQty} value={modalQty} />
-        <button type='submit'>Change Quantity</button>
-      </form>
+      <div className='fixed top-0 left-0 right-0 bottom-0 z-40' onClick={onClose} />
+      <div className="fixed top-2/4 left-2/4 bg-slate-400 translated p-2 z-40 rounded-2xl">
+        <div className='text-right'>
+          <button onClick={onClose}>
+            <FontAwesomeIcon icon={faXmark} />
+          </button>
+        </div>
+        <div>Currency: {coinId}</div>
+        <div className='mb-2'>Quantity: {quantity}</div>
+        <form onSubmit={handleModalSubmit} className='mb-2'>
+          <input type="number" step='any' onChange={handleModalQty} value={modalQty} className='p-1 rounded-lg' />
+          <button type='submit' className='btn-sm btn-primary mt-2'>Change Quantity</button>
+        </form>
+      </div>
     </div>
   );
 }
